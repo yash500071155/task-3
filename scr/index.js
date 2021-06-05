5c01173d687fdd3709febee8e298f00ef81ec9f0
yash500071155 <75775134+yash500071155@users.noreply.github.com>
2021-06-05 09:55:12 +0530

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -1,22 +1,14 @@
-  
 import React from "react";
-import { useSelector } from "react-redux";
-import CitySelection from "./Component/CitySelection";
-import Icon from "./Component/Icon";
-import WeatherView from "./Component/WeatherView";
-import { CityName } from "./redux/Weather/Slice";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/store";
 
-function App() {
-      Const City = useSelector(CityName);
-      return (
-         <div
-            ClassName = 'd-flex flex-column min-vh-100 p-5'
-            style= {{ backgroundColour: "black" }}
-          >
-            <Icon align='left' />
-              {city ? <WeatherView city={city} /> : <CitySelection/>}
-              <Icon align='right' />
-                </div>
-              );
-}
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
